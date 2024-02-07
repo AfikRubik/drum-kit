65d816c353f6abcfcecd21c775e42fb9c4cad7e7
AfikRubik <dzulfikar.af@gmail.com>
2024-02-07 07:58:06 +0700

    JavaScript done

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key='${e.key}']`);
+  const key = document.querySelector(`.key[data-key='${e.key}']`);
+  if (!audio) return;
+  console.log(e.key);
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add('playing');
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
